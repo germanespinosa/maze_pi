ffc95b0e33a57997c042c73a74a9c5e04587db8b
germanespinosa <germanespinosa@gmail.com>
2021-01-15 22:36:53 +0000

    none

--- feeder.py
+++ feeder.py
@@ -20,18 +20,18 @@ def feeder_process(feeder):
                 break
 
 class Feeder:
-    def __init__(self, feed_time):
+    def __init__(self, feed_time, feeder_number):
         self.feeding_time = feed_time #60ms
-        self.sensor = Button (17)
-        self.number = 1 
+        self.sensor = Button(17)
+        self.number = feeder_number
         self.solenoid = LED(27)
         self.active = False
         if not self.sensor.is_pressed:
-            self.sensor = Button (22)
+            self.sensor = Button(22)
             self.number = 2
 
     def status(self):
-        return {"state": "enabled" if self.active else "disabled", "feeding_time": self.feeding_time}
+        return {"feeder_number": self.number, "state": "enabled" if self.active else "disabled", "feeding_time": self.feeding_time}
 
     def test(self, feed_time, reps, wait_time):
         for i in range(reps):
@@ -41,7 +41,8 @@ class Feeder:
     def save_calibration(self, feed_time):
         self.feeding_time = feed_time
         with open("feeder.cal", "w") as f:
-            f.write(str(self.feeding_time))
+            f.write(str(self.feeding_time) + "\n")
+            f.write(str(self.number) + "\n")
 
     def report_feeder(self):
       uri = "http://192.168.137.1"
@@ -57,10 +58,13 @@ class Feeder:
 
 
 feed_time = 0
+feeder_number = 0
 if path.exists("feeder.cal"):
     with open("feeder.cal", "r") as f:
-        feed_time = float(f.readlines()[0].replace("\n", ""))
+        lines = f.readlines()
+        feed_time = float(lines[0].replace("\n", ""))
+        feeder_number = int(lines[1].replace("\n", ""))
 
-singleton = Feeder(feed_time)
+singleton = Feeder(feed_time,feeder_number)
 singleton_thread = start_new_thread(feeder_process, (singleton,))
 
